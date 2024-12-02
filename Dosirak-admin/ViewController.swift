72fb2250ae7b28872f7342a1761f87b6b01b5978
gomminjae <gommj0611@naver.com>
2024-12-03 04:24:33 +0900

    initial commit

---
+++ Dosirak-admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dosirak-admin
+//
+//  Created by 권민재 on 12/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
